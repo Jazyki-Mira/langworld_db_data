65fe41969fc9e8a0562360805fd4cbb2d623659f
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-04-03 14:05:06 +0300

    commit first version of `CustomValueLister` and its output
    this class creates lists of custom values in feature
    profiles as two Markdown files: grouped by volume
    / language and grouped by feature.

---
+++ langworld_db_data/validators/custom_value_lister.py
@@ -0,0 +1,132 @@
+from pathlib import Path
+
+from langworld_db_data.constants.paths import (
+    DISCUSSION_FILE_BY_DOCULECT,
+    FEATURE_PROFILES_DIR,
+    FILE_WITH_DOCULECTS,
+    FILE_WITH_NAMES_OF_FEATURES, DISCUSSION_FILE_BY_FEATURE
+)
+from langworld_db_data.filetools.csv_xls import read_csv, read_dict_from_2_csv_columns
+
+
+class CustomValueLister:
+    def __init__(self, dir_with_feature_profiles: Path = FEATURE_PROFILES_DIR):
+        self.doculect_ru_for_doculect_id = read_dict_from_2_csv_columns(
+            FILE_WITH_DOCULECTS, 'id', 'name_ru'
+        )
+
+        self.feature_ru_for_feature_id = read_dict_from_2_csv_columns(
+            FILE_WITH_NAMES_OF_FEATURES, 'id', 'ru'
+        )
+
+        self.encyclopedia_volume_for_doculect_id = read_dict_from_2_csv_columns(
+            FILE_WITH_DOCULECTS, 'id', 'encyclopedia_volume_id'
+        )
+
+        # replace empty values with zeroes for sorting
+        for key in self.encyclopedia_volume_for_doculect_id:
+            if not self.encyclopedia_volume_for_doculect_id[key]:
+                self.encyclopedia_volume_for_doculect_id[key] = '0'
+
+        self.custom_rows_for_volume_doculect_id = {}
+
+        list_of_files = sorted(
+            list(dir_with_feature_profiles.glob('*.csv')),
+            key=lambda f: (int(self.encyclopedia_volume_for_doculect_id[f.stem]), f.stem)
+        )
+
+        for file in list_of_files:
+            key = f'{self.encyclopedia_volume_for_doculect_id[file.stem]}:{file.stem}'
+            self.custom_rows_for_volume_doculect_id[key] = [
+                row for row in read_csv(file, read_as='dicts')
+                if row['value_type'] == 'custom'
+            ]
+
+    def write_grouped_by_volume_and_doculect(self):
+        content = ''
+        current_volume = ''
+
+        for volume_doculect_id in self.custom_rows_for_volume_doculect_id:
+            if not self.custom_rows_for_volume_doculect_id[volume_doculect_id]:
+                continue
+
+            volume, doculect_id = volume_doculect_id.split(':')[0], volume_doculect_id.split(':')[1]
+            if volume != current_volume:
+                content += f'## Том {volume}\n'
+                current_volume = volume
+
+            content += (
+                f'### [{self.doculect_ru_for_doculect_id[doculect_id]}]'
+                f'(../feature_profiles/{doculect_id}.csv)\n\n'
+            )
+            for row in self.custom_rows_for_volume_doculect_id[volume_doculect_id]:
+                content += (
+                    f'- **{row["feature_id"]}** ({self.feature_ru_for_feature_id[row["feature_id"]]}): '
+                    f'{row["value_ru"]}'
+                )
+                if row['comment_ru']:
+                    content += f'\n\n\t_Комментарий: {row["comment_ru"]}_'
+                content += '\n'
+            content += '\n'
+
+        # print(content)
+        with DISCUSSION_FILE_BY_DOCULECT.open(mode='w+', encoding='utf-8') as fh:
+            fh.write(content)
+
+    def write_grouped_by_feature(self):
+        rows_with_custom_values = []
+
+        for volume_doculect_id in self.custom_rows_for_volume_doculect_id:
+            if self.custom_rows_for_volume_doculect_id[volume_doculect_id]:
+                rows_with_custom_values += [
+                    [
+                        volume_doculect_id.split(':')[1],
+                        row['feature_id'],
+                        row['value_ru'],
+                        row['comment_ru']
+                    ]
+                    for row in self.custom_rows_for_volume_doculect_id[volume_doculect_id]
+                ]
+
+        rows_sorted_by_feature = sorted(
+            rows_with_custom_values,
+            key=lambda row: (
+                row[1].split('-')[0], # feature letter
+                int(row[1].split('-')[1]),  # feature number
+                row[2])
+        )
+
+        content = ''
+
+        current_feature = ''
+        current_value = ''
+
+        for row in rows_sorted_by_feature:
+            feature = row[1]
+            if feature != current_feature:
+                content += f'\n## {feature}: {self.feature_ru_for_feature_id[feature]}\n'
+                current_feature = feature
+                current_value = ''
+
+            value = row[2]
+
+            if value.lower() != current_value.lower():
+                content += (
+                    f'\n- {row[2]}: '
+                    f'[{self.doculect_ru_for_doculect_id[row[0]]}]'
+                    f'(../feature_profiles/{row[0]}.csv)'
+                )
+                current_value = value
+            else:
+                content += f', [{self.doculect_ru_for_doculect_id[row[0]]}](../feature_profiles/{row[0]}.csv)'
+
+        # print(content)
+
+        with DISCUSSION_FILE_BY_FEATURE.open(mode='w+', encoding='utf-8') as fh:
+            fh.write(content)
+
+
+if __name__ == '__main__':
+    lister = CustomValueLister()
+    lister.write_grouped_by_volume_and_doculect()
+    lister.write_grouped_by_feature()
