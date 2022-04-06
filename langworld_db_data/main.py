7dbabdccb602f46e073e2af824926a8d692ba9a9
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-04-06 11:23:21 +0300

    add main.py that runs all validation and Markdown generation

---
+++ langworld_db_data/main.py
@@ -0,0 +1,17 @@
+from langworld_db_data.markdown_generators.custom_value_lister import CustomValueLister
+from langworld_db_data.validators.doculect_inventory_validator import DoculectInventoryValidator
+from langworld_db_data.validators.feature_value_inventory_validator import FeatureValueInventoryValidator
+
+
+def main():
+    # These will also run during testing, but it doesn't hurt to check again
+    DoculectInventoryValidator().validate()
+    FeatureValueInventoryValidator().validate()
+
+    print('\nWriting Markdown files')
+    CustomValueLister().write_grouped_by_feature()
+    CustomValueLister().write_grouped_by_volume_and_doculect()
+
+
+if __name__ == '__main__':
+    main()
