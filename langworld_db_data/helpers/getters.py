861182d479e541bfa6d2916ed17943d5c838748e
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-05-25 14:17:15 +0300

    switch `get_value_for_doculect_and_feature` from CLDF to base file
    CLDF dataset (as of now) does not have entries
    for value types `not_stated` and `not_applicable`,
    which I need. Use doculect ID to open CSV feature
    profile of given doculect instead of finding
    this ID in CLDF dataset.
    
    Additional functionality:
    * allow to disable copying to clipboard
    * make print more informative
    * copy comment to clipboard if value is empty

--- langworld_db_data/helpers/getters.py
+++ langworld_db_data/helpers/getters.py
@@ -1,36 +1,45 @@
 import pyperclip
 
-from langworld_db_data.constants.paths import CLDF_DIR
+from langworld_db_data.constants.paths import FEATURE_PROFILES_DIR
 from langworld_db_data.filetools.csv_xls import read_csv
 
 
-def get_value_for_doculect_and_feature(doculect_id: str, feature_id: str) -> dict:
+def get_value_for_doculect_and_feature(
+        doculect_id: str, feature_id: str, copy_to_clipboard: bool = True
+) -> dict:
     """A helper function to get value for given feature in given doculect.
-    Prints and returns found value type, ID and text. **Copies value text to clipboard**.
+    Prints and returns found value type, ID and text.
+    **Copies value text to clipboard** (by default).
 
-    Can be handy for renaming custom/listed values
+    Can be handy when renaming custom/listed values
     when adding/unifying listed values.
-
-    Note that this function reads from the **CLDF dataset**,
-    not from CSV feature profiles.
 
     If copying to clipboard does not work on Ubuntu, run
     `sudo apt install xclip` first.
     """
 
-    values_file = CLDF_DIR / 'values.csv'
-    for row in read_csv(values_file, read_as='dicts'):
-        if row['Language_ID'] == doculect_id and row['Parameter_ID'] == feature_id:
-            value_type = 'listed' if row['Code_ID'] else 'custom'
-            data = {'value_type': value_type, 'value_id': row['Code_ID'], 'value': row['Value_RU']}
+    file = FEATURE_PROFILES_DIR / f'{doculect_id}.csv'
+    if not file.exists():
+        raise FileNotFoundError(f'Feature profile {doculect_id}.csv does not exist')
 
-            print('Value type: ', data['value_type'])
-            if data['value_type'] == 'listed':
-                print(data['value_id'])
-            print(data['value'])
+    print(f'{doculect_id=}, {feature_id=}\n')
 
-            pyperclip.copy(data['value'])
-            print('Value text copied to clipboard')
+    for row in read_csv(file, read_as='dicts'):
+        if row['feature_id'] == feature_id:
+            data = {key: row[key] for key in ('value_type', 'value_id', 'value_ru', 'comment_ru')}
+
+            for attr in ('value_type', 'value_id', 'value_ru', 'comment_ru'):
+                attr_for_print = attr.replace("_ru", "").replace("_", " ").capitalize().replace('id', 'ID')
+                if data[attr]:
+                    print(f'{attr_for_print}: {data[attr]}')
+                else:
+                    print(f'{attr_for_print} is empty')
+
+            # sometimes a custom value can be written in comment while value itself is left empty
+            text_to_copy = data['value_ru'] if data['value_ru'] else data['comment_ru']
+            if copy_to_clipboard and text_to_copy:
+                pyperclip.copy(text_to_copy)
+                print('\nValue (or comment for empty value) copied to clipboard')
 
             return data
 
