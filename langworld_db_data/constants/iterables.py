f76732b5d585861803d6cfd5f81cf0c985262155
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-05-28 15:53:06 +0300

    rm `iterables.py`
    so far it serves no purpose

--- langworld_db_data/constants/iterables.py
+++
@@ -1,4 +0,0 @@
-from langworld_db_data.constants.paths import FEATURE_PROFILES_DIR
-
-# tuple to make it immutable and hence suitable for a constant
-FEATURE_PROFILES = tuple(sorted(list(FEATURE_PROFILES_DIR.glob('*.csv'))))
