fe829d72dc0c3301d4a1446d8ade2b4caf0fb8d1
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-06-17 19:51:00 +0300

    move `countries.csv` to `inventories`, add paths to files with assets

--- langworld_db_data/constants/paths.py
+++ langworld_db_data/constants/paths.py
@@ -12,8 +12,12 @@ DISCUSSION_FILE_WITH_CUSTOM_VALUES_BY_FEATURE = DISCUSSION_DIR / 'custom_values_
 DISCUSSION_FILE_WITH_LISTED_VALUES = DISCUSSION_DIR / 'listed_values_by_feature.md'
 
 ASSETS_DIR = DATA_DIR / 'assets'
+FILE_WITH_MAP_TO_DOCULECT = ASSETS_DIR / 'encyclopedia_map_to_doculect.csv'
+FILE_WITH_MAPS = ASSETS_DIR / 'encyclopedia_maps.csv'
+FILE_WITH_ENCYCLOPEDIA_VOLUMES = ASSETS_DIR / 'encyclopedia_volumes.csv'
 
 INVENTORIES_DIR = DATA_DIR / 'inventories'
+FILE_WITH_COUNTRIES = INVENTORIES_DIR / 'countries.csv'
 FILE_WITH_CATEGORIES = INVENTORIES_DIR / 'feature_categories.csv'
 FILE_WITH_DOCULECTS = INVENTORIES_DIR / 'doculects.csv'
 FILE_WITH_NAMES_OF_FEATURES = INVENTORIES_DIR / 'features.csv'
@@ -26,7 +30,6 @@ FEATURE_PROFILES_DIR = DATA_DIR / 'feature_profiles'
 
 SOCIOLINGUISTIC_DIR = DATA_DIR / 'sociolinguistics'
 SOCIOLINGUISTIC_PROFILES_DIR = SOCIOLINGUISTIC_DIR / 'sociolinguistic_profiles'
-FILE_WITH_COUNTRIES = SOCIOLINGUISTIC_DIR / 'countries.csv'
 FILE_WITH_COUNTRY_ALIASES = SOCIOLINGUISTIC_DIR / 'country_aliases_for_profile_parsing.csv'
 FILE_WITH_DOCULECTS_MATCHED_TO_COUNTRIES = SOCIOLINGUISTIC_DIR / 'doculects_to_countries.csv'
 
