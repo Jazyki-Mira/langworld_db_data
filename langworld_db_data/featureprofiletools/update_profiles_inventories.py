7e983d57988ee0442bcd3487155f7ec1a46a7092
mari-an-shum <mari.an.shum@mail.ru>
2024-04-30 18:41:23 +0300

    rename variable for the sake of clarity

--- langworld_db_data/featureprofiletools/update_profiles_inventories.py
+++ langworld_db_data/featureprofiletools/update_profiles_inventories.py
@@ -1,7 +1,7 @@
 from langworld_db_data.constants.paths import FEATURE_PROFILES_DIR, FILE_WITH_LISTED_VALUES
 
 
-def rename_value(value_id: str, new_value_name: str,
+def rename_value(value_to_rename_id: str, new_value_name: str,
                  feature_profiles_dir, file_with_listed_values: str):
     """
     Replaces all the instances of a given value name in profiles and features_listed_values on a given value.
@@ -20,17 +20,17 @@ def rename_value(value_id: str, new_value_name: str,
         with open(file, "w") as f:
             for line in data_from_file:
                 line = line.split()
-                if value_id in line[4]:
-                    if line[4] == value_id:
+                if value_to_rename_id in line[4]:
+                    if line[4] == value_to_rename_id:
                         line[4] = new_value_name
                         number_of_replacements += 1
                     elif "&" in line[4]:
                         combined_value_ids = line[3].split("&")
                         combined_value_name = line[4].split("&")
                         for i in range(len(combined_value_ids)):
-                            if combined_value_ids[i] == value_id:
+                            if combined_value_ids[i] == value_to_rename_id:
                                 combined_value_name[i] = new_value_name
                                 number_of_replacements += 1
                 f.write(",".join(line))
     if number_of_replacements == 0:
-        print("Nothing has been replaced. Maybe you entered a wrong value_id.")
+        print("Nothing has been replaced. Maybe you entered a wrong value_to_rename_id.")
