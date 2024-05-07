6c8cae10742599f05bae1decb9183ff9e01b06ec
mari-an-shum <mari.an.shum@mail.ru>
2024-05-07 14:09:09 +0300

    improve test

--- langworld_db_data/featureprofiletools/update_profiles_inventories.py
+++ langworld_db_data/featureprofiletools/update_profiles_inventories.py
@@ -2,44 +2,49 @@ from pathlib import Path
 
 from langworld_db_data.constants.paths import FEATURE_PROFILES_DIR, FILE_WITH_LISTED_VALUES
 
-from langworld_db_data.filetools.csv_xls import read_dicts_from_csv
+from langworld_db_data.filetools.csv_xls import read_dicts_from_csv, write_csv
 
 
 def rename_value(
-    value_to_rename_id: str, new_value_name: str, feature_profiles_dir: Path, file_with_listed_values: Path
+    value_to_rename_id: str, new_value_name: str, feature_profiles_dir=FEATURE_PROFILES_DIR,
+        file_with_listed_values=FILE_WITH_LISTED_VALUES
 ):
     """
     Replaces all the instances of a given value name in profiles and features_listed_values on a given value.
     Works with both singular and combined values.
-    Argument feature_profiles_dir is transmitted temporarily, for convenience of tests. It will later be replaced with
-    FEATURE_PROFILES_DIR. The same goes for file_with_listed_values.
     """
     number_of_replacements = 0
-    files_list = list(feature_profiles_dir.iterdir())
+    replacement_made = False
+    files_list = list(feature_profiles_dir.glob("*.csv"))
     for file in files_list:
-        print("Replacements made:" + str(number_of_replacements))
+        print("Opening " + file.name)
+        data_from_file = []
         data_from_file = read_dicts_from_csv(file)
         for line in data_from_file:
             if value_to_rename_id in line["value_id"]:
-                print("Found in " + str(file))
-                print(line)
                 if line["value_id"] == value_to_rename_id:
+                    print("Found exact match in " + file.name)
+                    print("Changed " + line["value_ru"] + " to " + new_value_name)
                     line["value_ru"] = new_value_name
                     number_of_replacements += 1
+                    replacement_made = True
                 elif "&" in line["value_id"]:
+                    print("Found match in combined value in " + file.name)
                     combined_value_ids = line["value_id"].split("&")
                     combined_value_names = line["value_ru"].split("&")
                     for i in range(len(combined_value_ids)):
                         if combined_value_ids[i] == value_to_rename_id:
                             combined_value_names[i] = new_value_name
                             number_of_replacements += 1
+                            replacement_made = True
                     line["value_ru"] = "&".join(combined_value_names)
-        with open(file, "w", encoding="utf-8") as f:
+        print("Replacements made:" + str(number_of_replacements))
+        if replacement_made:
             for line in data_from_file:
-                try:
-                    f.write(",".join(str(item) for item in line.items()))
-                except TypeError:
-                    print("Unable to write line: " + ",".join(str(item) for item in line.items()))
+                print(line)
+            replacement_made = False
+        write_csv(data_from_file, file, overwrite=True, delimiter=",")
+        print("Successfully written into csv-file")
     data_from_file = read_dicts_from_csv(file_with_listed_values)
     for line in data_from_file:
         if line["id"] == value_to_rename_id:
--- tests/test_featureprofiletools_update_profiles_inventories.py
+++ tests/test_featureprofiletools_update_profiles_inventories.py
@@ -1,34 +1,21 @@
-import pytest
-
 from langworld_db_data.featureprofiletools.update_profiles_inventories import rename_value
 
 from tests.paths import DIR_WITH_FEATURE_PROFILE_TOOLS_TEST_FILES
+
+from tests.test_helpers import check_existence_of_output_csv_file_and_compare_with_gold_standard
 
 DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES = (
     DIR_WITH_FEATURE_PROFILE_TOOLS_TEST_FILES / "update_profiles_inventories"
 )
 
-DIR_WITH_DEFAULT_FILES = (
-    DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / "default"
+DIR_WITH_INPUT_FILES = (
+    DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / "input"
 )
 
-DIR_WITH_FILES_CHANGED = (
-    DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / "changed"
+DIR_WITH_OUTPUT_GOLD_STANDARD_FILES = (
+    DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / "output_gold_standard"
 )
 
-DIR_WITH_TEMPORARY_FILES = (
-    DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / "tmp"
-)
-
-DIR_WITH_DEFAULT_FEATURE_PROFILES = (
-    DIR_WITH_DEFAULT_FILES / "feature_profiles"
-)
-
-DIR_WITH_DEFAULT_INVENTORIES = (
-    DIR_WITH_DEFAULT_FILES / "inventories"
-)
-
-PATH_TO_FILE_WITH_DEFAULT_FEATURES_LISTED_VALUES = DIR_WITH_DEFAULT_INVENTORIES / "features_listed_values.csv"
 
 # Currently I am working with values only, but I put features_listed_values.csv in this folder where features.csv will
 # be added later.
@@ -36,38 +23,34 @@ PATH_TO_FILE_WITH_DEFAULT_FEATURES_LISTED_VALUES = DIR_WITH_DEFAULT_INVENTORIES 
 
 def test_rename_value():
     # First change files in tmp up to their default variants
-    filenames_list = DIR_WITH_DEFAULT_FEATURE_PROFILES.glob('*.csv')
+    filepaths_list = DIR_WITH_INPUT_FILES.glob('*.csv')
+
     file_lines = []
-    for filename in filenames_list:
-        print(filename)
-        with open(DIR_WITH_DEFAULT_FEATURE_PROFILES / filename, "r", encoding="utf-8") as f:
+    for filepath in filepaths_list:
+        print("Opening " + filepath.name)
+        with open(DIR_WITH_INPUT_FILES / filepath.name, "r", encoding="utf-8") as f:
             for line in f:
                 file_lines.append(line)
-        with open(DIR_WITH_TEMPORARY_FILES / filename, "w", encoding="utf-8") as f:
+        with open(DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / filepath.name, "w", encoding="utf-8") as f:
             for line in file_lines:
                 f.write(line)
-    with open(PATH_TO_FILE_WITH_DEFAULT_FEATURES_LISTED_VALUES, "r", encoding="utf-8") as f:
-        for line in f:
-            file_lines.append(line)
-    with open(DIR_WITH_TEMPORARY_FILES / "features_listed_values.csv", "w", encoding="utf-8") as f:
-        for line in file_lines:
-            f.write(line)
+    print("Successfully put files into their default versions")
                 
     rename_value(
         value_to_rename_id="A-9-2",
         new_value_name="Представлены исключительно дифтонги",
-        feature_profiles_dir=DIR_WITH_TEMPORARY_FILES,
-        file_with_listed_values=DIR_WITH_TEMPORARY_FILES / "features_listed_values.csv"
+        feature_profiles_dir=DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES,
+        file_with_listed_values=DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / "features_listed_values.csv"
     )
     
-    temporary_filenames_list = DIR_WITH_TEMPORARY_FILES.glob('*.csv')
+    output_filenames_list = DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES.glob('*.csv')
     file_lines_in_result = []  # How the function changed files
     file_lines_reference = []  # How files should have been changed
-    for filename in temporary_filenames_list:
-        with open(DIR_WITH_TEMPORARY_FILES / filename, "r", encoding="utf-8"):
+    for filename in output_filenames_list:
+        with open(DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES / filename, "r", encoding="utf-8"):
             for line in f:
                 file_lines_in_result.append(line)
-        with open(DIR_WITH_FILES_CHANGED / filename, "r", encoding="utf-8"):
+        with open(DIR_WITH_OUTPUT_GOLD_STANDARD_FILES / filename, "r", encoding="utf-8"):
             for line in f:
                 file_lines_reference.append(line)
     assert file_lines_in_result == file_lines_reference
