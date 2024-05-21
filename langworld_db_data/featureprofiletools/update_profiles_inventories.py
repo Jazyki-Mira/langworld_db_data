a7471ffbe26e74b5e8ad460624e5f6b02c8e445a
mari-an-shum <mari.an.shum@mail.ru>
2024-05-21 14:48:09 +0300

    remove spaces between constants in their declaration, remove an outdated commentary

--- langworld_db_data/featureprofiletools/update_profiles_inventories.py
+++ langworld_db_data/featureprofiletools/update_profiles_inventories.py
@@ -22,7 +22,7 @@ def rename_value_in_profiles_and_inventories(
         update_one_feature_profile(
             id_of_value_to_rename=id_of_value_to_rename,
             new_value_name=new_value_name,
-            input_filepath=file,
+            input_file=file,
             output_dir=output_feature_profiles_dir,
         )
 
@@ -31,7 +31,7 @@ def rename_value_in_profiles_and_inventories(
     update_features_listed_values(
         id_of_value_to_rename=id_of_value_to_rename,
         new_value_name=new_value_name,
-        input_filepath=input_inventories_dir / "features_listed_values.csv",
+        input_file=input_inventories_dir / "features_listed_values.csv",
         output_filepath=output_inventories_dir / "features_listed_values.csv",
     )
 
@@ -39,12 +39,12 @@ def rename_value_in_profiles_and_inventories(
 def update_one_feature_profile(
     id_of_value_to_rename: str,
     new_value_name: str,
-    input_filepath: Path,
+    input_file: Path,
     output_dir: Path,
 ) -> None:
 
     number_of_replacements = 0
-    data_from_file = read_dicts_from_csv(input_filepath)
+    data_from_file = read_dicts_from_csv(input_file)
     data_to_write = []
     for line in data_from_file:
         if id_of_value_to_rename not in line["value_id"]:
@@ -53,40 +53,44 @@ def update_one_feature_profile(
         line_to_write = line.copy()
         # After this clause, only lines with the target value are considered (they may contain other values too)
         if "&" in line["value_id"]:
-            print("Found match in combined value in " + input_filepath.name)
+            print("Found match in combined value in " + input_file.name)
             combined_value_ids = line["value_id"].split("&")
             target_value_index = combined_value_ids.index(id_of_value_to_rename)
             combined_value_names = line["value_ru"].split("&")
             combined_value_names.pop(target_value_index)
             combined_value_names.insert(target_value_index, new_value_name)
             line_to_write["value_ru"] = "&".join(combined_value_names)
+            number_of_replacements += 1
             data_to_write.append(line_to_write)
             print("Changed " + line["value_ru"] + " to " + line_to_write["value_ru"])
             continue
         # After this clause, only those lines are considered which contain the target value only
-        print(f"Found exact match in {input_filepath.name}")
+        print(f"Found exact match in {input_file.name}")
         line_to_write["value_ru"] = new_value_name
         number_of_replacements += 1
         data_to_write.append(line_to_write)
         print("Changed " + line["value_ru"] + " to " + new_value_name)
     print("Replacements made in this file:" + str(number_of_replacements))
-    output_file = output_dir / input_filepath.name
+    output_file = output_dir / input_file.name
     write_csv(rows=data_to_write, path_to_file=output_file, overwrite=True, delimiter=",")
     print(f"Successfully written to {output_file}")
 
 
 def update_features_listed_values(
-    id_of_value_to_rename: str, new_value_name: str, input_filepath: Path, output_filepath: Path
+    id_of_value_to_rename: str,
+    new_value_name: str,
+    input_file: Path,
+    output_filepath: Path
 ) -> None:
 
-    data_from_file = read_dicts_from_csv(input_filepath)
+    data_from_file = read_dicts_from_csv(input_file)
     data_to_write = []
     for line in data_from_file:
         if id_of_value_to_rename not in line["id"]:
             data_to_write.append(line)
             continue
         line_to_write = line.copy()
-        print("Found exact match in " + input_filepath.name)
+        print("Found exact match in " + input_file.name)
         print("Changed " + line["ru"] + " to " + new_value_name)
         line_to_write["ru"] = new_value_name
         data_to_write.append(line_to_write)
