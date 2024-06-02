5918db2cb48778dfdf783cb6ae17991bc8505699
MariAnShum <162987222+MariAnShum@users.noreply.github.com>
2024-06-02 10:28:18 +0300

    make fixture in test shorter
    Co-authored-by: Dmitry K <58207913+lemontree210@users.noreply.github.com>

--- tests/test_featureprofiletools_value_renamer.py
+++ tests/test_featureprofiletools_value_renamer.py
@@ -21,7 +21,7 @@ DIR_WITH_OUTPUT_GOLD_STANDARD_INVENTORIES = DIR_WITH_OUTPUT_GOLD_STANDARD_FILES 
 
 @pytest.fixture
 def value_renamer():
-    value_renamer = ValueRenamer(
+    return ValueRenamer(
         input_feature_profiles_dir=DIR_WITH_INPUT_FEATURE_PROFILES,
         output_feature_profiles_dir=DIR_WITH_TEST_UPDATE_PROFILES_INVENTORIES,
         input_inventories_dir=DIR_WITH_INPUT_INVENTORIES,
