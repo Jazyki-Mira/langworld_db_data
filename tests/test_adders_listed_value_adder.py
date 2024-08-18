ef4079cb1a9d2e86562db38c70ae002766fd428d
mari-an-shum <mari.an.shum@mail.ru>
2024-08-18 13:05:51 +0300

    reorder tests to replicate order of methods in listed_value_adder

--- tests/test_adders_listed_value_adder.py
+++ tests/test_adders_listed_value_adder.py
@@ -73,20 +73,6 @@ def test__add_to_inventory_of_listed_values_append_to_end_with_custom_values(tes
         )
 
 
-def test__add_to_inventory_of_listed_values_put_as_first(test_adder):
-    test_adder.add_listed_value(
-        feature_id="A-3",
-        new_value_en="Central, mid-back and back",
-        new_value_ru="Средний, задне-средний и задний",
-        index_to_assign=1,
-    )
-
-    check_existence_of_output_csv_file_and_compare_with_gold_standard(
-        output_file=test_adder.output_file_with_listed_values,
-        gold_standard_file=GS_FILE_WITH_LISTED_VALUES_INSERTION_AS_FIRST,
-    )
-
-
 def test__add_to_inventory_of_listed_values_throws_exception_with_zero_index_to_assign(test_adder):
     with pytest.raises(ListedValueAdderError, match="must be between 1 and 4"):
         test_adder.add_listed_value(
@@ -184,6 +170,20 @@ def test__add_to_inventory_of_listed_values_append_to_end_with_explicit_index_no
     )
 
 
+def test__add_to_inventory_of_listed_values_put_as_first(test_adder):
+    test_adder._add_to_inventory_of_listed_values(
+        feature_id="A-3",
+        new_value_en="Central, mid-back and back",
+        new_value_ru="Средний, задне-средний и задний",
+        index_to_assign=1,
+    )
+
+    check_existence_of_output_csv_file_and_compare_with_gold_standard(
+        output_file=test_adder.output_file_with_listed_values,
+        gold_standard_file=GS_FILE_WITH_LISTED_VALUES_INSERTION_AS_FIRST,
+    )
+
+
 def test__add_to_inventory_of_listed_values_insert_after_non_final_value(test_adder):
     new_value_id = test_adder._add_to_inventory_of_listed_values(
         feature_id="A-3",
@@ -200,46 +200,17 @@ def test__add_to_inventory_of_listed_values_insert_after_non_final_value(test_ad
     )
 
 
-def test__mark_value_as_listed_in_feature_profiles(test_adder):
-    test_adder._mark_value_as_listed_in_feature_profiles(
-        feature_id="A-11",
-        new_value_id="A-11-15",
-        new_value_ru="Есть первые, вторые и третьи",
-        custom_values_to_rename=[
-            "первые, вторые и третьи",  # it is also lowercase in feature profile
-            "третьи, вторые и первые",  # first word is capitalized in feature profile
-            "Первые, третьи и вторые",
-        ],
-    )
-
-    for stem in STEMS_OF_EXPECTED_OUTPUT_FILES:
-        assert (OUTPUT_DIR_FOR_LISTED_VALUE_ADDER_FEATURE_PROFILES / f"{stem}.csv").exists(), (
-            f"File {stem}.csv was not created. It means that no changes were made while"
-            " there should have been changes"
-        )
-
-    for file in OUTPUT_DIR_FOR_LISTED_VALUE_ADDER_FEATURE_PROFILES.glob("*.csv"):
-        assert (
-            file.stem not in STEMS_OF_FILES_THAT_MUST_NOT_BE_CHANGED
-        ), f"File {file.name} is not supposed to be changed"
-
-        print(f"TEST: checking amended feature profile {file.name}")
-
-        check_existence_of_output_csv_file_and_compare_with_gold_standard(
-            output_file=file,
-            gold_standard_file=GS_DIR_WITH_FEATURE_PROFILES_AFTER_ADDITION / file.name,
-        )
-
-
 def test__get_indices_and_their_line_numbers_in_features_listed_values(test_adder):
     rows = [
         {"id": "A-1-1", "feature_id": "A-1", "en": "Two", "ru": "Две"},
         {"id": "A-2-1", "feature_id": "A-2", "en": "Close and open", "ru": "Верхний и нижний"},
         {"id": "A-2-2", "feature_id": "A-2", "en": "Close and mid", "ru": "Верхний и средний"},
     ]
-    value_indices_to_inventory_line_numbers = test_adder._get_indices_and_their_line_numbers_for_given_feature_in_inventory_of_listed_values(
-        rows=rows,
-        feature_id="A-2",
+    value_indices_to_inventory_line_numbers = (
+        test_adder._get_indices_and_their_line_numbers_for_given_feature_in_inventory_of_listed_values(
+            rows=rows,
+            feature_id="A-2",
+        )
     )
 
     assert value_indices_to_inventory_line_numbers == (
@@ -290,3 +261,34 @@ def test__increment_ids_whose_indices_are_not_less_than_index_to_assign_in_rows(
     ]
 
     assert rows == gold_standard_rows
+
+
+def test__mark_value_as_listed_in_feature_profiles(test_adder):
+    test_adder._mark_value_as_listed_in_feature_profiles(
+        feature_id="A-11",
+        new_value_id="A-11-15",
+        new_value_ru="Есть первые, вторые и третьи",
+        custom_values_to_rename=[
+            "первые, вторые и третьи",  # it is also lowercase in feature profile
+            "третьи, вторые и первые",  # first word is capitalized in feature profile
+            "Первые, третьи и вторые",
+        ],
+    )
+
+    for stem in STEMS_OF_EXPECTED_OUTPUT_FILES:
+        assert (OUTPUT_DIR_FOR_LISTED_VALUE_ADDER_FEATURE_PROFILES / f"{stem}.csv").exists(), (
+            f"File {stem}.csv was not created. It means that no changes were made while"
+            " there should have been changes"
+        )
+
+    for file in OUTPUT_DIR_FOR_LISTED_VALUE_ADDER_FEATURE_PROFILES.glob("*.csv"):
+        assert (
+            file.stem not in STEMS_OF_FILES_THAT_MUST_NOT_BE_CHANGED
+        ), f"File {file.name} is not supposed to be changed"
+
+        print(f"TEST: checking amended feature profile {file.name}")
+
+        check_existence_of_output_csv_file_and_compare_with_gold_standard(
+            output_file=file,
+            gold_standard_file=GS_DIR_WITH_FEATURE_PROFILES_AFTER_ADDITION / file.name,
+        )
