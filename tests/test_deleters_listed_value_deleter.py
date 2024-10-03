462525c5e51f814c9118a81e00e90961f46d1caa
Marina <mari.an.shum@yandex.ru>
2024-10-03 12:32:20 +0300

    write a CSI draft of the test

--- tests/test_deleters_listed_value_deleter.py
+++ tests/test_deleters_listed_value_deleter.py
@@ -3,4 +3,6 @@
 # and subsequently update the IDs of reminiscent values
 # Moreover, as was discussed earlier, it should not simply delete values but pop them
 # so that the popped value will be available to insert somewhere else with ListedValueAdder
-# How should the deleted values be changed in profiles? Should they become custom or not stated?
+# How should the deleted values be changed in profiles? Should they become custom or not_stated?
+# To me, it would be more logical to make them custom, again for the sake of possible movement
+# However, perhaps both options may be necessary to provide
