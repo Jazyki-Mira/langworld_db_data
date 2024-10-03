4344016d218df613d41a9fae9e679cea77709600
Marina <mari.an.shum@yandex.ru>
2024-10-03 12:30:22 +0300

    write a CSI draft of the test

---
+++ tests/test_deleters_listed_value_deleter.py
@@ -0,0 +1,6 @@
+# To my mind, this will probably be a structure isomorphic to adders
+# It should be, first of all, able to delete a value from the inventory
+# and subsequently update the IDs of reminiscent values
+# Moreover, as was discussed earlier, it should not simply delete values but pop them
+# so that the popped value will be available to insert somewhere else with ListedValueAdder
+# How should the deleted values be changed in profiles? Should they become custom or not stated?
