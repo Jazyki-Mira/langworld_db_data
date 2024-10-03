76565f2d74377dac239f7e684d178860bee30930
Marina <mari.an.shum@yandex.ru>
2024-10-03 13:03:59 +0300

    unfold functional structure for listed_value_remover

--- langworld_db_data/removers/listed_value_remover.py
+++ langworld_db_data/removers/listed_value_remover.py
@@ -7,9 +7,16 @@ class ListedValueRemoverError(RemoverError):
 class ListedValueRemover(Remover):
     def remove_listed_value(self):
         self._remove_from_inventory_of_listed_values()
+        self._remove_from_feature_profiles()
 
     def _remove_from_inventory_of_listed_values(self):
-        pass
+        self._update_value_ids_in_inventory()
 
     def _remove_from_feature_profiles(self):
+        pass
+
+    def _update_value_ids_in_inventory(self):
+        self._update_value_ids_in_feature_profiles()
+
+    def _update_value_ids_in_feature_profiles(self):
         pass
