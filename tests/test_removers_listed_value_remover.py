a3bcc394079ed2db7dda4cfce6a33b99a1906786
Marina <mari.an.shum@yandex.ru>
2024-10-03 13:04:41 +0300

    unfold test structure for listed_value_remover

---
+++ tests/test_removers_listed_value_remover.py
@@ -0,0 +1,40 @@
+import pytest
+
+from langworld_db_data.removers.listed_value_remover import ListedValueRemover, ListedValueRemoverError
+from tests.helpers import check_existence_of_output_csv_file_and_compare_with_gold_standard
+
+
+# To my mind, this will probably be a structure isomorphic to adders
+# It should be, first of all, able to remove a value from the inventory
+# and subsequently update the IDs of reminiscent values
+# Moreover, as was discussed earlier, it should not simply remove values but pop them
+# so that the popped value will be available to insert somewhere else with ListedValueAdder
+
+
+@pytest.fixture(scope="function")
+def test_remover():
+    return ListedValueRemover()
+
+
+# Tests
+def test_remove_listed_value_from_end_of_feature(test_remover):
+    # Check if the target value is removed from the inventory (the very end of the feature) and saved into a variable
+    # A-5-8 should be removed
+    pass
+
+
+# Check if the target value is removed from the inventory (not the end of the feature) and saved into a variable
+# A-5-5 should be removed
+
+
+# Check the validity of the requested value_id
+
+
+# What should the module do if it is asked to pop a value which does not exist?
+
+
+# How should the removed values be changed in profiles? Should they become custom or not_stated?
+# To me, it would be more logical to make them custom, again for the sake of possible movement
+# However, perhaps both options may be necessary to provide
+
+# Should we also be able to remove custom values?
