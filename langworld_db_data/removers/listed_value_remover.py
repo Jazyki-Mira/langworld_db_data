38452e6723b97f7e2499943260d26ffff856b504
Marina <mari.an.shum@yandex.ru>
2024-10-17 11:37:07 +0300

    WIP

--- langworld_db_data/removers/listed_value_remover.py
+++ langworld_db_data/removers/listed_value_remover.py
@@ -1,4 +1,6 @@
 from langworld_db_data.removers.remover import Remover, RemoverError
+from langworld_db_data.constants.literals import ID_SEPARATOR
+from langworld_db_data.filetools.csv_xls import read_dicts_from_csv, write_csv
 
 
 class ListedValueRemoverError(RemoverError):
@@ -10,7 +12,9 @@ class ListedValueRemover(Remover):
         self,
         id_of_value_to_remove: str,
     ) -> str:
-        removed_value_information = self._remove_from_inventory_of_listed_values()
+        removed_value_information = self._remove_from_inventory_of_listed_values(
+            id_of_value_to_remove=id_of_value_to_remove
+        )
         self._remove_from_feature_profiles()
 
         if not removed_value_information:
@@ -21,9 +25,28 @@ class ListedValueRemover(Remover):
         else:
             return removed_value_information
 
-    def _remove_from_inventory_of_listed_values(self) -> str:
+    def _remove_from_inventory_of_listed_values(
+            self,
+            id_of_value_to_remove: str,
+    ) -> str:
         removed_value_information = ""
+
+        rows = read_dicts_from_csv(self.input_file_with_listed_values)
+        for i, row in enumerate(rows):
+            if row["id"] == id_of_value_to_remove:
+                break
+
         self._update_value_ids_in_inventory()
+
+        rows_without_removed_value =
+
+        write_csv(
+            rows_with_new_value_inserted,
+            path_to_file=self.output_file_with_listed_values,
+            overwrite=True,
+            delimiter=",",
+        )
+
         return removed_value_information
 
     def _remove_from_feature_profiles(self):
