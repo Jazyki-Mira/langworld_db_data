6ca3c71c171b904ddce41a007302d1ca82a57d5b
Marina <mari.an.shum@yandex.ru>
2024-12-19 13:36:40 +0300

    write tests for movers

--- tests/test_movers_listed_value_mover.py
+++ tests/test_movers_listed_value_mover.py
@@ -11,6 +11,8 @@ from tests.paths import (
 
 GS_DIR_WITH_MOVERS_FEATURE_PROFILES = DIR_WITH_MOVERS_FEATURE_PROFILES / "gold_standard"
 
+GS_FILE_FOR_MOVING_FROM_MIDDLE_TO_DIFFERENT_MIDDLE_UP = (DIR_WITH_MOVERS_TEST_FILES /
+    "features_listed_values_gold_standard_from_middle_to_different_middle_up.csv")
 
 @pytest.fixture(scope="function")
 def test_mover():
@@ -22,9 +24,47 @@ def test_mover():
         output_dir_with_feature_profiles=OUTPUT_DIR_FOR_LISTED_VALUE_MOVER_FEATURE_PROFILES,
     )
 
-
+# move from A-3-4 o A-3-2
 def test_move_listed_value(test_mover):
-    pass
+
+    stems_of_files_that_must_be_changed = [
+        "catalan",
+        "franco_provencal",
+        "pashto",
+    ]
+
+    stems_of_files_that_must_not_be_changed = [
+        "corsican",
+        "ukrainian",
+    ]
+
+    test_mover.move_listed_value(
+        initial_value_id="A-3-4",
+        final_value_id="A-3-2",
+    )
+
+    check_existence_of_output_csv_file_and_compare_with_gold_standard(
+        output_file=test_mover.output_file_with_listed_values,
+        gold_standard_file=GS_FILE_FOR_MOVING_FROM_MIDDLE_TO_DIFFERENT_MIDDLE_UP,
+    )
+
+    for stem in stems_of_files_that_must_be_changed:
+        assert (test_mover.output_dir_with_feature_profiles / f"{stem}.csv").exists(), (
+            f"File {stem}.csv was not created. It means that no changes were made while"
+            " there should have been changes"
+        )
+
+    for file in test_mover.output_dir_with_feature_profiles.glob("*.csv"):
+        assert (
+            file.stem not in stems_of_files_that_must_not_be_changed
+        ), f"File {file.name} is not supposed to be changed"
+
+        print(f"TEST: checking amended feature profile {file.name}")
+
+        check_existence_of_output_csv_file_and_compare_with_gold_standard(
+            output_file=file,
+            gold_standard_file=GS_DIR_WITH_MOVERS_FEATURE_PROFILES / file.name,
+        )
 
 
 def test__move_value_in_inventory_of_listed_values(test_mover):
@@ -34,4 +74,8 @@ def test__move_value_in_inventory_of_listed_values(test_mover):
 def test_move_value_in_inventory_of_listed_values_throws_error_with_coinciding_from_and_to(
     test_mover,
 ):
-    pass
+    with pytest.raises(ListedValueMoverError, match="must not coincide"):
+        test_mover.move_listed_value(
+            initial_value_id="A-3-2",
+            final_value_id="A-3-2",
+        )
