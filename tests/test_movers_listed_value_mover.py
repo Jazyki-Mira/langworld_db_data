34ab27c26564ffdb81d4de8a9d543d88ff874030
Marina <mari.an.shum@yandex.ru>
2024-12-19 13:06:06 +0300

    create file with tests for listed_value_mover

---
+++ tests/test_movers_listed_value_mover.py
@@ -0,0 +1,26 @@
+import pytest
+
+from langworld_db_data.movers.listed_value_mover import (
+    ListedValueMover,
+    ListedValueMoverError,
+)
+from tests.helpers import check_existence_of_output_csv_file_and_compare_with_gold_standard
+from tests.paths import (
+    DIR_WITH_MOVERS_TEST_FILES,
+    INPUT_FILE_WITH_LISTED_VALUES_FOR_MOVERS,
+    DIR_WITH_MOVERS_FEATURE_PROFILES,
+    OUTPUT_DIR_FOR_LISTED_VALUE_MOVER_FEATURE_PROFILES,
+)
+
+GS_DIR_WITH_MOVERS_FEATURE_PROFILES = DIR_WITH_MOVERS_FEATURE_PROFILES / "gold_standard"
+
+
+@pytest.fixture(scope="function")
+def test_mover():
+    return ListedValueMover(
+        input_file_with_listed_values=INPUT_FILE_WITH_LISTED_VALUES_FOR_MOVERS,
+        output_file_with_listed_values=DIR_WITH_MOVERS_TEST_FILES
+        / "features_listed_values_output_value_remover.csv",
+        input_dir_with_feature_profiles=DIR_WITH_MOVERS_FEATURE_PROFILES,
+        output_dir_with_feature_profiles=OUTPUT_DIR_FOR_LISTED_VALUE_MOVER_FEATURE_PROFILES,
+    )
