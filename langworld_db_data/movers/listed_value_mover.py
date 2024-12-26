e890f8591aeae139287b39de84c189ee3dc392f7
Marina Shumilina <mari.an.shum@iling-ran.ru>
2024-12-26 12:55:35 +0300

    WIP

--- langworld_db_data/movers/listed_value_mover.py
+++ langworld_db_data/movers/listed_value_mover.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from langworld_db_data.adders.listed_value_adder import ListedValueAdder
 from langworld_db_data.constants.literals import ID_SEPARATOR
 from langworld_db_data.filetools.csv_xls import read_dicts_from_csv, write_csv
