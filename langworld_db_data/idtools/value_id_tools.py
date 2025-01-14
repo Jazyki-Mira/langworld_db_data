6b20d871aa7e0095372656a55213c9efb62ca511
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-01-14 13:05:00 +0300

    write text description of methods that must be coded

---
+++ langworld_db_data/idtools/value_id_tools.py
@@ -0,0 +1,15 @@
+"""
+tools for ID should be able to do:
+- feature ID extraction
+- value index extraction
+- value ID composition from feature ID and value index
+- ID decomposition into the three components -- category ID, feature index and
+value index
+All these are frequently used in methods for adding/removing/moving values.
+- value ID incrementation
+- value ID decrementation
+These are used only in adding and removing values once each, but perhaps
+writing corresponding methods may improve the readability of the methods
+
+Perhaps these methods might yield a new class later
+"""
