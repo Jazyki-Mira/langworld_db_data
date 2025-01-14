6b58219d5ba26ab66a175e02fd7641bf0652eacf
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-01-14 13:16:41 +0300

    write tests for extraction

--- tests/test_idtools.py
+++ tests/test_idtools.py
@@ -1,6 +1,13 @@
+from langworld_db_data.idtools.value_id_tools import (extract_feature_id,
+                                                      extract_value_index)
+
+
+ID_FOR_EXTRACTION = "A-12-6"
+
+
 def test_extract_feature_id():
-    pass
+    assert extract_feature_id(ID_FOR_EXTRACTION) == "A-12"
 
 
 def test_extract_value_index():
-    pass
+    assert extract_value_index(ID_FOR_EXTRACTION) == "6"
