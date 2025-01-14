decff3e143689af81dd7a89ca3accd208366c580
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-01-14 13:48:56 +0300

    add tests for feature index and category ID extraction

--- tests/test_idtools.py
+++ tests/test_idtools.py
@@ -1,23 +1,24 @@
 from langworld_db_data.idtools.value_id_tools import (
+    extract_category_id,
     extract_feature_id,
+    extract_feature_index,
     extract_value_index,
 )
 
 IDS_FOR_EXTRACTION = [
     'A-1-2',
-    'A-1-22',
-    'A-12-6',
-    'A-12-26',
+    'B-7-22',
+    'C-12-6',
+    'K-14-26',
 ]
-# add cases with different numbers of digits
 
 
 def test_extract_feature_id():
     gold_standard = [
         'A-1',
-        'A-1',
-        'A-12',
-        'A-12',
+        'B-7',
+        'C-12',
+        'K-14',
     ]
     for i in range(len(IDS_FOR_EXTRACTION)):
         assert extract_feature_id(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
@@ -27,3 +28,15 @@ def test_extract_value_index():
     gold_standard = [2, 22, 6, 26]
     for i in range(len(IDS_FOR_EXTRACTION)):
         assert extract_value_index(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+
+
+def test_extract_category_id():
+    gold_standard = ['A', 'B', 'C', 'K']
+    for i in range(len(IDS_FOR_EXTRACTION)):
+        assert extract_category_id(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+
+
+def test_extract_feature_index():
+    gold_standard = [1, 7, 12, 14]
+    for i in range(len(IDS_FOR_EXTRACTION)):
+        assert extract_feature_index(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
