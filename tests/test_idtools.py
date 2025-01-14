03f467c492c731fe717115f47cf7bcea68c192a8
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-01-14 13:18:43 +0300

    adjust tests

--- tests/test_idtools.py
+++ tests/test_idtools.py
@@ -1,13 +1,19 @@
 from langworld_db_data.idtools.value_id_tools import (extract_feature_id,
-                                                      extract_value_index)
+                                                      extract_value_index_as_int,
+                                                      extract_value_index_as_str,
+                                                      )
 
 
-ID_FOR_EXTRACTION = "A-12-6"
+ID_FOR_EXTRACTION = 'A-12-6'
 
 
 def test_extract_feature_id():
-    assert extract_feature_id(ID_FOR_EXTRACTION) == "A-12"
+    assert extract_feature_id(ID_FOR_EXTRACTION) == 'A-12'
 
 
-def test_extract_value_index():
-    assert extract_value_index(ID_FOR_EXTRACTION) == "6"
+def test_extract_value_index_as_str():
+    assert extract_value_index_as_str(ID_FOR_EXTRACTION) == '6'
+
+
+def test_extract_value_index_as_int():
+    assert extract_value_index_as_int(ID_FOR_EXTRACTION) == 6
