633fd74a6698d938bafb9ed20e099a118db0045f
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-01-14 14:59:44 +0300

    write test for function for splitting IDs

--- tests/test_idtools.py
+++ tests/test_idtools.py
@@ -1,9 +1,11 @@
 from langworld_db_data.idtools.value_id_tools import (
+    _split_value_id,
     extract_category_id,
     extract_feature_id,
     extract_feature_index,
     extract_value_index,
 )
+
 
 IDS_FOR_EXTRACTION = [
     "A-1-2",
@@ -11,6 +13,17 @@ IDS_FOR_EXTRACTION = [
     "C-12-6",
     "K-14-26",
 ]
+
+
+def test__split_value_id():
+    gold_standard = [
+        ["A", "1", "2"],
+        ["B", "7", "22"],
+        ["C", "12", "6"],
+        ["K", "14", "26"],
+    ]
+    for i in range(len(IDS_FOR_EXTRACTION)):
+        assert _split_value_id(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
 
 
 def test_extract_feature_id():
