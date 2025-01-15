983b962c90de7efa0d07ae6549e8b65cdee266dc
Marina Shumilina <mari.an.shum@yandex.ru>
2025-01-15 16:01:17 +0300

    rewrite loops in tests using zip

--- tests/test_idtools.py
+++ tests/test_idtools.py
@@ -21,8 +21,8 @@ def test__split_value_id():
         ["C", "12", "6"],
         ["K", "14", "26"],
     )
-    for i in zip(IDS_FOR_EXTRACTION):
-        assert _split_value_id(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+    for id_to_gold_standard in zip(IDS_FOR_EXTRACTION, gold_standard):
+        assert _split_value_id(id_to_gold_standard[0]) == id_to_gold_standard[1]
 
 
 def test_extract_feature_id():
@@ -32,23 +32,23 @@ def test_extract_feature_id():
         "C-12",
         "K-14",
     )
-    for i in zip(IDS_FOR_EXTRACTION):
-        assert extract_feature_id(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+    for value_id_to_gold_standard in zip(IDS_FOR_EXTRACTION, gold_standard):
+        assert extract_feature_id(value_id_to_gold_standard[0]) == value_id_to_gold_standard[1]
 
 
 def test_extract_value_index():
     gold_standard = (2, 22, 6, 26)
-    for i in zip(IDS_FOR_EXTRACTION):
-        assert extract_value_index(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+    for value_id_to_gold_standard in zip(IDS_FOR_EXTRACTION, gold_standard):
+        assert extract_value_index(value_id_to_gold_standard[0]) == value_id_to_gold_standard[1]
 
 
 def test_extract_category_id():
     gold_standard = ("A", "B", "C", "K")
-    for i in zip(IDS_FOR_EXTRACTION):
-        assert extract_category_id(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+    for value_id_to_gold_standard in zip(IDS_FOR_EXTRACTION, gold_standard):
+        assert extract_category_id(value_id_to_gold_standard[0]) == value_id_to_gold_standard[1]
 
 
 def test_extract_feature_index():
     gold_standard = (1, 7, 12, 14)
-    for i in zip(IDS_FOR_EXTRACTION):
-        assert extract_feature_index(IDS_FOR_EXTRACTION[i]) == gold_standard[i]
+    for value_id_to_gold_standard in zip(IDS_FOR_EXTRACTION, gold_standard):
+        assert extract_feature_index(value_id_to_gold_standard[0]) == value_id_to_gold_standard[1]
