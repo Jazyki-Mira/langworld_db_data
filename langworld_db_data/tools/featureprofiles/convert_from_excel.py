c18ea6e75ed0bbfa5bdb1ce46b3b4671da019c8f
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2025-01-22 08:26:52 +0300

    refactor: function to class

--- langworld_db_data/tools/featureprofiles/convert_from_excel.py
+++ langworld_db_data/tools/featureprofiles/convert_from_excel.py
@@ -12,89 +12,94 @@ from langworld_db_data.tools.files.csv_xls import read_dicts_from_xls
 from langworld_db_data.tools.files.json_toml_yaml import read_json_toml_yaml
 
 
-def convert_from_excel(path_to_input_excel: Path) -> Path:
-    """Converts Excel "questionnaire" file (which researchers produce)
-    into feature profile in CSV. Returns output path for convenience.
+class FeatureProfileConverter:
+    def excel_to_csv(self, path_to_input_excel: Path) -> Path:
+        """Converts Excel "questionnaire" file (which researchers produce)
+        into feature profile in CSV. Returns output path for convenience.
 
-    The most notable differences between input Excel and output CSV:
+        The most notable differences between input Excel and output CSV:
 
-    1. Excel questionnaire has two columns for value name
-    (one for 'listed' and one for 'custom'), whereas a CSV file has only one.
+        1. Excel questionnaire has two columns for value name
+        (one for 'listed' and one for 'custom'), whereas a CSV file has only one.
 
-    2. In multiselect features, Excel file has multiple lines
-    for each elementary value (because of dropdown fields) while CSV file
-    has one line with all elementary values joined together.
-    """
+        2. In multiselect features, Excel file has multiple lines
+        for each elementary value (because of dropdown fields) while CSV file
+        has one line with all elementary values joined together.
+        """
 
-    # It is important to always use the current YAML file in conversion,
-    # so I did not include path to it into parameters to allow overriding it in tests
-    file_with_names = CONFIG_DIR / "excel_sheet_and_column_names.yaml"
-    sheet_or_column_name_for_id = read_json_toml_yaml(file_with_names)
-    # for mypy
-    if not isinstance(sheet_or_column_name_for_id, dict):
-        raise TypeError(
-            f"File with names of Excel sheet and columns {file_with_names} was supposed"
-            " to be read as a dictionary. Please check the file."
-        )
-
-    rows = read_dicts_from_xls(
-        path_to_file=path_to_input_excel,
-        sheet_name=sheet_or_column_name_for_id["sheet_name"],
-    )
-
-    # We could do without using custom class here, but it improves readability,
-    # plus FeatureProfileWriterFromDictionary has a method for writing such dict to CSV.
-    value_for_feature_id: dict[str, ValueForFeatureProfileDictionary] = {}
-
-    processed_feature_ids: set[str] = set()
-    """A set for keeping track of feature IDs already processed.
-    Allows to add another value ID and value name if we encounter more than one row for a multiselect feature.
-    """
-
-    for row in rows:
-        _get = partial(_get_value_from_row, row_=row, name_for_id=sheet_or_column_name_for_id)
-
-        feature_id = _get("feature_id")
-        value_type = _get("value_type")
-        value_id = _get("value_id")
-        value_ru = (
-            _get("listed_value_ru").removeprefix(f"{value_id}: ")
-            if value_type == "listed"
-            else _get("custom_value_ru")
-        )
-
-        # if this is a new feature ID, just write the value
-        if feature_id not in processed_feature_ids:
-            value_for_feature_id[feature_id] = ValueForFeatureProfileDictionary(
-                feature_name_ru=_get("feature_name_ru"),
-                value_type=value_type,
-                value_id=value_id,
-                value_ru=value_ru,
-                comment_ru=_get("comment_ru"),
-                comment_en=_get("comment_en"),
-                page_numbers=_get("page_numbers"),
+        # It is important to always use the current YAML file in conversion,
+        # so I did not include path to it into parameters to allow overriding it in tests
+        file_with_names = CONFIG_DIR / "excel_sheet_and_column_names.yaml"
+        sheet_or_column_name_for_id = read_json_toml_yaml(file_with_names)
+        # for mypy
+        if not isinstance(sheet_or_column_name_for_id, dict):
+            raise TypeError(
+                f"File with names of Excel sheet and columns {file_with_names} was supposed"
+                " to be read as a dictionary. Please check the file."
             )
-            processed_feature_ids.add(feature_id)
-        else:
-            # otherwise add the value to the existing dictionary entry
-            value_for_feature_id[feature_id].value_id += f"&{value_id}"
-            value_for_feature_id[feature_id].value_ru += f"&{value_ru}"
 
-    output_path = path_to_input_excel.parent / f"{path_to_input_excel.stem}.csv"
-    print(f"Saving converted Excel file as {output_path}")
-    FeatureProfileWriterFromDictionary.write(
-        feature_dict=value_for_feature_id, output_path=output_path
-    )
-    return output_path
+        rows = read_dicts_from_xls(
+            path_to_file=path_to_input_excel,
+            sheet_name=sheet_or_column_name_for_id["sheet_name"],
+        )
 
+        # We could do without using custom class here, but it improves readability,
+        # plus FeatureProfileWriterFromDictionary has a method for writing such dict to CSV.
+        value_for_feature_id: dict[str, ValueForFeatureProfileDictionary] = {}
 
-def _get_value_from_row(column_id: str, row_: dict[str, str], name_for_id: dict[str, str]) -> str:
-    """Returns value from column with relevant name.
-    Name of column is looked up by `attr` in YAML file."""
-    return row_[name_for_id[column_id]]
+        processed_feature_ids: set[str] = set()
+        """A set for keeping track of feature IDs already processed.
+        Allows to add another value ID and value name if we encounter more than one row for a multiselect feature.
+        """
+
+        for row in rows:
+            _get = partial(
+                self._get_value_from_row, row_=row, name_for_id=sheet_or_column_name_for_id
+            )
+
+            feature_id = _get("feature_id")
+            value_type = _get("value_type")
+            value_id = _get("value_id")
+            value_ru = (
+                _get("listed_value_ru").removeprefix(f"{value_id}: ")
+                if value_type == "listed"
+                else _get("custom_value_ru")
+            )
+
+            # if this is a new feature ID, just write the value
+            if feature_id not in processed_feature_ids:
+                value_for_feature_id[feature_id] = ValueForFeatureProfileDictionary(
+                    feature_name_ru=_get("feature_name_ru"),
+                    value_type=value_type,
+                    value_id=value_id,
+                    value_ru=value_ru,
+                    comment_ru=_get("comment_ru"),
+                    comment_en=_get("comment_en"),
+                    page_numbers=_get("page_numbers"),
+                )
+                processed_feature_ids.add(feature_id)
+            else:
+                # otherwise add the value to the existing dictionary entry
+                value_for_feature_id[feature_id].value_id += f"&{value_id}"
+                value_for_feature_id[feature_id].value_ru += f"&{value_ru}"
+
+        output_path = path_to_input_excel.parent / f"{path_to_input_excel.stem}.csv"
+        print(f"Saving converted Excel file as {output_path}")
+        FeatureProfileWriterFromDictionary.write(
+            feature_dict=value_for_feature_id, output_path=output_path
+        )
+        return output_path
+
+    @staticmethod
+    def _get_value_from_row(
+        column_id: str, row_: dict[str, str], name_for_id: dict[str, str]
+    ) -> str:
+        """Returns value from column with relevant name.
+        Name of column is looked up by `attr` in YAML file."""
+        return row_[name_for_id[column_id]]
 
 
 if __name__ == "__main__":
     for file in (Path(__file__).parent.resolve() / "input").glob("*.xlsm"):
         print(f"Converting {file.name}")
-        convert_from_excel(file)
+        FeatureProfileConverter().excel_to_csv(file)
