856413867a63c866218d6612a2eb8d6edb77de5e
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-05-06 15:24:43 +0300

    remove arguments from listed_value_adder

--- langworld_db_data/adders/listed_value_adder.py
+++ langworld_db_data/adders/listed_value_adder.py
@@ -336,8 +336,7 @@ class ListedValueAdder(ObjectWithPaths):
 
 if __name__ == "__main__":
     ListedValueAdder().add_listed_value(
-        feature_id="A-6",
-        new_value_en="Present for back vowels",
-        new_value_ru="В заднем ряду",
-        index_to_assign=4,
+        feature_id="",
+        new_value_en="",
+        new_value_ru="",
     )
