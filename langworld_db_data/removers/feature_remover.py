fa8ec2d142bd18bc36786159430cf9550d069859
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-05-22 13:35:45 +0300

    write dummy functions

---
+++ langworld_db_data/removers/feature_remover.py
@@ -0,0 +1,15 @@
+class FeatureRemoverError(Exception):
+    pass
+
+class FeatureRemover:
+    def remove_feature():
+        pass
+
+    def _remove_from_inventory_of_features():
+        pass
+
+    def _remove_from_inventory_of_listed_values():
+        pass
+
+    def _remove_from_feature_profiles_and_update_ids_whose_indices_are_greater_than_one_of_removed_value():
+        pass
