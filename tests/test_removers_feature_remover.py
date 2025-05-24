4e5176fe3e11eded93922bd89321aee2bd289b3e
Marina Shumilina <mari.an.shum@yandex.ru>
2025-05-24 16:33:42 +0300

    write tests for remove_feature method

--- tests/test_removers_feature_remover.py
+++ tests/test_removers_feature_remover.py
@@ -940,4 +940,29 @@ def test__remove_from_feature_profiles_remove_from_the_end_of_category(test_remo
 
 
 def test_remove_feature(test_remover):
-    pass
+    test_remover.remove_feature(
+        feature_id="N-3"
+    )
+
+    check_existence_of_output_csv_file_and_compare_with_gold_standard(
+        output_file=test_remover.output_file_with_features,
+        gold_standard_file= DIR_WITH_GOLD_STANDARD_FILES / "features_without_N_3.csv",
+    )
+
+    check_existence_of_output_csv_file_and_compare_with_gold_standard(
+        output_file=test_remover.output_file_with_listed_values,
+        gold_standard_file=DIR_WITH_GOLD_STANDARD_FILES / "features_listed_values_without_N_3.csv",
+    )
+
+    gold_standard_feature_profiles = list(
+        (DIR_WITH_GOLD_STANDARD_FILES / "feature_profiles_without_N_3").glob("*.csv")
+    )
+
+    for file in gold_standard_feature_profiles:
+        test_output_file = test_remover.output_dir_with_feature_profiles / file.name
+
+        check_existence_of_output_csv_file_and_compare_with_gold_standard(
+            output_file=test_output_file,
+            gold_standard_file=file,
+        )
+
