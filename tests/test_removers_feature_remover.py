1019860f327de3bfc7a235b6e0c629286bcbf862
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2025-06-10 11:23:25 +0300

    FeatureRemover

--- tests/test_removers_feature_remover.py
+++ tests/test_removers_feature_remover.py
@@ -1,6 +1,6 @@
 import pytest
 
-from langworld_db_data.removers.feature_remover import FeatureRemover, FeatureRemoverError
+from langworld_db_data.tools.features.feature_remover import FeatureRemover, FeatureRemoverError
 from tests.helpers import check_existence_of_output_csv_file_and_compare_with_gold_standard
 from tests.paths import (
     DIR_WITH_REMOVERS_TEST_FILES,
