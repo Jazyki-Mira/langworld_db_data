78bdfb94653e5084cd61a72cedf255936c254b29
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2025-06-15 08:57:12 +0300

    listed_value_remover.py

--- langworld_db_data/tools/listed_values/listed_value_remover.py
+++ langworld_db_data/tools/listed_values/listed_value_remover.py
@@ -146,6 +146,4 @@ class ListedValueRemover(ObjectWithPaths):
 
 
 if __name__ == "__main__":
-    ListedValueRemover().remove_listed_value(
-        id_of_value_to_remove="",
-    )
+    ListedValueRemover().remove_listed_value(id_of_value_to_remove="")  # pragma: no cover
