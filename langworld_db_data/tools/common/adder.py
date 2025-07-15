bc8dcc35afee922c27500497a8d13c7adf55da4f
Marina Shumilina <mari.an.shum@iling-ran.ru>
2025-07-15 17:13:22 +0300

    minor changes in Adder

--- langworld_db_data/tools/common/adder.py
+++ langworld_db_data/tools/common/adder.py
@@ -1,6 +1,18 @@
 from typing import Literal
 
 from langworld_db_data import ObjectWithPaths
+from langworld_db_data.tools.common.ids.compose import (
+    compose_feature_id,
+    compose_value_id_from_scratch,
+)
+
+
+FeatureOrValue = Literal["feature", "value"]
+CategoryOrFeature = Literal["category", "feature"]
+
+
+class AdderError(Exception):
+    pass
 
 
 class Adder(ObjectWithPaths):
@@ -12,7 +24,7 @@ class Adder(ObjectWithPaths):
     def _check_if_index_to_assign_is_in_list_of_applicable_indices(
         self,
         index_to_validate: int,
-        category_or_feature: Literal["category", "feature"],
+        category_or_feature: CategoryOrFeature,
         category_or_feature_id: str,
     ) -> bool:
         pass
@@ -20,7 +32,7 @@ class Adder(ObjectWithPaths):
 
     def _get_range_of_currently_existing_indices(
         self,
-        category_or_feature: Literal["category", "feature"],
+        category_or_feature: CategoryOrFeature,
         category_or_feature_id: str,
     ) -> tuple[int]:
         pass
@@ -28,7 +40,7 @@ class Adder(ObjectWithPaths):
 
     def _make_id_for_new_feature_or_value(
         self,
-        feature_or_value: Literal["feature", "value"],
+        feature_or_value: FeatureOrValue,
         new_feature_or_value_id: str,
         index_to_assign: int,
     ) -> str:
@@ -37,7 +49,7 @@ class Adder(ObjectWithPaths):
 
     def _compose_new_row(
         self,
-        feature_or_value: Literal["feature", "value"],
+        feature_or_value: FeatureOrValue,
         args_of_new_feature_or_value: tuple[str],
     ) -> dict[str, str]:
         pass
@@ -45,9 +57,9 @@ class Adder(ObjectWithPaths):
 
     def _get_line_number_where_to_insert(
         self,
-        feature_or_value: Literal["feature", "value"],
+        feature_or_value: FeatureOrValue,
         new_feature_or_value_id: str,
-        category_or_feature: Literal["category", "feature"],
+        category_or_feature: CategoryOrFeature,
         category_or_feature_id: str,
     ) -> int:
         pass
