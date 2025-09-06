c9358fce2e9c518ea93e6e90631a30dd9beffc3b
Marina Shumilina <mari.an.shum@yandex.ru>
2025-09-06 12:56:31 +0300

    add new extraction function extract_last_index

--- langworld_db_data/tools/common/ids/__init__.py
+++ langworld_db_data/tools/common/ids/__init__.py
@@ -1,6 +1,7 @@
 from .extract import _split_value_id  # not including in __all__
 from .extract import (
     extract_category_id,
+    extract_last_index,
     extract_feature_id,
     extract_feature_index,
     extract_value_index,
@@ -8,6 +9,7 @@ from .extract import (
 from .update import decrement_indices_after_deletion
 
 __all__ = [
+    "extract_last_index",
     "extract_feature_id",
     "extract_feature_index",
     "extract_value_index",
