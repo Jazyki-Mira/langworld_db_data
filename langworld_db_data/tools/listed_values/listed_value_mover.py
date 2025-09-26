2e53ed1a6d790b69e63b5ce2087a319f8ae5dbef
Marina Shumilina <mari.an.shum@yandex.ru>
2025-09-26 11:54:57 +0300

    put B-4-2 in the first place

--- langworld_db_data/tools/listed_values/listed_value_mover.py
+++ langworld_db_data/tools/listed_values/listed_value_mover.py
@@ -57,6 +57,6 @@ class ListedValueMover(ObjectWithPaths):
 
 if __name__ == "__main__":
     ListedValueMover().move_listed_value(
-        initial_value_id="",
-        index_to_assign=0,
+        initial_value_id="B-4-2",
+        index_to_assign=1,
     )  # pragma: no cover
