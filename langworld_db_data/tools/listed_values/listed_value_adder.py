84200c489d477e874577f3456f66fd5746b2a977
Marina Shumilina <mari.an.shum@yandex.ru>
2025-12-18 10:50:37 +0300

    add new D-2-9 and run main

--- langworld_db_data/tools/listed_values/listed_value_adder.py
+++ langworld_db_data/tools/listed_values/listed_value_adder.py
@@ -355,8 +355,8 @@ class ListedValueAdder(ObjectWithPaths):
 
 if __name__ == "__main__":
     ListedValueAdder().add_listed_value(
-        feature_id="D-1",
+        feature_id="D-2",
         new_value_en="No long (geminate) consonants",
         new_value_ru="Запрет на  долгие (геминированные) согласные",
-        index_to_assign=8,
+        index_to_assign=9,
     )  # pragma: no cover
