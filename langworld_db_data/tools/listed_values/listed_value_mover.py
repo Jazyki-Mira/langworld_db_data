24df5e25cee734803fe90437f35143c4540d8b83
Marina Shumilina <mari.an.shum@yandex.ru>
2025-12-18 10:37:18 +0300

    move initial D-2-13 (current D-2-12) to position 5 and run main

--- langworld_db_data/tools/listed_values/listed_value_mover.py
+++ langworld_db_data/tools/listed_values/listed_value_mover.py
@@ -57,6 +57,6 @@ class ListedValueMover(ObjectWithPaths):
 
 if __name__ == "__main__":
     ListedValueMover().move_listed_value(
-        initial_value_id="D-1-12",
+        initial_value_id="D-2-12",
         index_to_assign=5,
     )  # pragma: no cover
