b61ccabe5cabb99d74b84862571e12ea7acd871f
Marina Shumilina <mari.an.shum@yandex.ru>
2025-12-25 09:26:10 +0300

    resolve combination of J-7-12 (initial J-7-8), remove it from inventory and run main

--- langworld_db_data/tools/listed_values/listed_value_remover.py
+++ langworld_db_data/tools/listed_values/listed_value_remover.py
@@ -164,4 +164,4 @@ class ListedValueRemover(ObjectWithPaths):
 
 
 if __name__ == "__main__":
-    ListedValueRemover().remove_listed_value(id_of_value_to_remove="J-7-13")  # pragma: no cover
+    ListedValueRemover().remove_listed_value(id_of_value_to_remove="J-7-12")  # pragma: no cover
